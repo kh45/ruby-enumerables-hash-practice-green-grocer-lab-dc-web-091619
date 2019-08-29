1764cecad43c6ca603c5e555e9aaef07cbf7a2f4
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-29 13:58:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,39 @@
 def consolidate_cart(cart)
-  # code here
+  before_final = cart.map{|hsh| hsh.to_a}.flatten
+  new_hash = Hash[*before_final]
+  for items in before_final do
+    if items.class == String
+      new_hash[items][:count] = before_final.count(items)
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each{|item|
+    if cart.keys.include?(item[:item])
+      cart["#{item[:item]} W/COUPON"] = {
+        :price => item[:cost] / item[:num], 
+        :clearance => cart[item[:item]][:clearance], 
+        :count => item[:num] * coupons.count(item)}
+      cart[item[:item]][:count] -= item[:num]
+    end
+    }
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each{|key, value|
+    value[:price] = (value[:price] * 0.8).round(2) if value[:clearance]
+    }
 end
 
 def checkout(cart, coupons)
-  # code here
+  final_cart = consolidate_cart(cart)
+  apply_coupons(final_cart, coupons)
+  apply_clearance(final_cart)
+  total = 0
+  final_cart.each{|key, value|
+    total += value[:price] * value[:count]}
+  total
 end
